442892ca5edcf4371c7f73888bedc62c17a761d1
Michael Sproul <michael@sigmaprime.io>
2020-11-11 10:15:25 +1100

    Fix head_epoch script

--- head_epoch.fish
+++ head_epoch.fish
@@ -1,4 +1,4 @@
 #!/usr/bin/env fish
 
-set head_slot (curl -s "http://localhost:5052/eth/v1/node/syncing" | jq .data.head_slot)
+set head_slot (curl -s "http://localhost:5052/eth/v1/node/syncing" | jq ".data.head_slot | tonumber")
 echo (math "floor($head_slot / 32)")
