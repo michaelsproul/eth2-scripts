4718559ec362c69a2750fd528db76a65402d1b6b
Michael Sproul <michael@sigmaprime.io>
2021-07-07 11:20:52 +1000

    Count eth1 votes more precisely

--- eth1_votes/analyse.py
+++ eth1_votes/analyse.py
@@ -1,23 +1,28 @@
 #!/usr/bin/env python3
 
 import json
+from collections import namedtuple
 
-def count_vote(block_hash, votes):
-    return len([1 for vote in votes if vote["block_hash"] == block_hash])
+Eth1DataVote = namedtuple("Eth1DataVote", ["deposit_root", "deposit_count", "block_hash"])
+
+def eth1_data_vote(v):
+    return Eth1DataVote(v["deposit_root"], int(v["deposit_count"]), v["block_hash"])
+
+def count_vote(target_vote, votes):
+    return len([1 for vote in votes if vote == target_vote])
 
 state = json.load(open("head_state.json"))
-votes = state["data"]["eth1_data_votes"]
-slot = int(state["data"]["slot"])
+votes = [eth1_data_vote(vote) for vote in state["data"]["eth1_data_votes"]]
+uniq_votes = set(votes)
+vote_counts = {vote: count_vote(vote, votes) for vote in uniq_votes}
+
 current_block_hash = state["data"]["eth1_data"]["block_hash"]
-block_hashes = set(v["block_hash"] for v in votes)
-uniq_votes = {block_hash: count_vote(block_hash, votes) for block_hash in block_hashes}
 
-for (block_hash, count) in sorted(uniq_votes.items(), key=lambda v: -v[1]):
-    if block_hash == current_block_hash:
-        print("{}: {} (incumbent)".format(block_hash, count))
-    else:
-        print("{}: {}".format(block_hash, count))
+for (vote, count) in sorted(vote_counts.items(), key=lambda v: (-v[1], v[0].block_hash, -v[0].deposit_count)):
+    description = " (incumbent)" if vote.block_hash == current_block_hash else ""
+    print("{}[{}]: {}{}".format(vote.block_hash, vote.deposit_count, count, description))
 
+slot = int(state["data"]["slot"])
 slots_remaining = 2048 - (slot % 2048) - 1
 print("total votes in period: {}, slots remaining: {}".format(len(votes), slots_remaining))
 print("participation: {}%".format(round(100 * len(votes) / (slot % 2048), 2)))
