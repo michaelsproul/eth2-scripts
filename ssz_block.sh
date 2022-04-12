824233b7c7ac78af4242a04c4e22621e28e9ce75
Michael Sproul <michael@sigmaprime.io>
2022-04-12 12:45:55 +1000

    Use /v2/ URLs for SSZ blocks and states

--- ssz_block.sh
+++ ssz_block.sh
@@ -2,4 +2,4 @@
 
 source $(eth2_scripts_dir.sh)/setup.sh
 
-curl -H "Accept: application/octet-stream" "$BEACON_NODE/eth/v1/beacon/blocks/$1"
+curl -H "Accept: application/octet-stream" "$BEACON_NODE/eth/v2/beacon/blocks/$1"
--- ssz_state.sh
+++ ssz_state.sh
@@ -2,4 +2,4 @@
 
 source $(eth2_scripts_dir.sh)/setup.sh
 
-curl -H "Accept: application/octet-stream" "$BEACON_NODE/eth/v1/debug/beacon/states/$1"
+curl -H "Accept: application/octet-stream" "$BEACON_NODE/eth/v2/debug/beacon/states/$1"
