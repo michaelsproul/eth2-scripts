f4680dd44d092b7e9509e16d6bea07393d411c71
Michael Sproul <michael@sigmaprime.io>
2022-09-09 10:39:19 +1000

    Enhance JWT engine script

--- engine_syncing.sh
+++ engine_syncing.sh
@@ -1,11 +1,26 @@
 #!/usr/bin/env bash
 
-cat /tmp/jwtsecret | xxd -r -p > /tmp/jwtsecret_bin
-JWT=$(jwt encode --secret @/tmp/jwtsecret_bin)
+set -eo pipefail
+
+if [ $# -eq 0 ]
+then
+    echo "no JWT secret provided"
+    echo "usage: ./engine_syncing.sh JWT [URL]"
+    exit 1
+fi
+
+jwtsecret=$1
+tmpfile=./.engine_syncing_sh_jwt
+
+engine_url=$2
+engine_url=${engine_url:="http://localhost:8551"}
+
+cat $jwtsecret | xxd -r -p > $tmpfile
+JWT=$(jwt encode --secret @$tmpfile)
 
 curl \
     --location \
-    --request POST 'http://localhost:8551/' \
+    --request POST $engine_url \
     --header 'Content-Type: application/json' \
     --header "Authorization: Bearer $JWT" \
     --data-raw "{
@@ -14,3 +29,5 @@ curl \
         \"params\": [],
         \"id\": 1
     }"
+
+rm -f $tmpfile
