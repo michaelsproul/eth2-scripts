8c76ec52694d598af500b6969f1e7baf8127f906
Michael Sproul <michael@sigmaprime.io>
2023-06-27 17:41:35 +1000

    Engine capabilities script

---
+++ engine_capabilities.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+set -eo pipefail
+
+if [ $# -eq 0 ]
+then
+    echo "no JWT secret provided"
+    echo "usage: ./engine_capabilites.sh JWT [URL]"
+    exit 1
+fi
+
+jwtsecret=$1
+tmpfile=./.engine_capabilites_sh_jwt
+
+engine_url=$2
+engine_url=${engine_url:="http://localhost:8551"}
+
+cat $jwtsecret | xxd -r -p > $tmpfile
+JWT=$(jwt encode --secret @$tmpfile)
+
+curl \
+    --location \
+    --request POST $engine_url \
+    --header 'Content-Type: application/json' \
+    --header "Authorization: Bearer $JWT" \
+    --data-raw "{
+        \"jsonrpc\": \"2.0\",
+        \"method\": \"engine_exchangeCapabilities\",
+        \"params\": [[\"engine_getPayloadBodiesByRangeV1\"]],
+        \"id\": 1
+    }"
+
+rm -f $tmpfile
