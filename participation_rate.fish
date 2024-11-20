69ae1576f8b8df8d16d79d072727139bc5406b38
Michael Sproul <michael@sigmaprime.io>
2024-11-21 09:12:23 +1100

    Update participation rate script

--- participation_rate.fish
+++ participation_rate.fish
@@ -7,15 +7,11 @@ set target_epoch (math "$argv[1] + 1")
 
 set global_votes (curl --fail --show-error -s "http://localhost:5052/lighthouse/validator_inclusion/$target_epoch/global"; or exit)
 set global_votes (echo $global_votes | jq .data)
-set prev_active_gwei (echo $global_votes | jq .previous_epoch_active_gwei)
-set prev_target_gwei (echo $global_votes | jq .previous_epoch_target_attesting_gwei)
-set curr_active_gwei (echo $global_votes | jq .current_epoch_active_gwei)
-set participation_rate_current (math "$prev_target_gwei / $prev_active_gwei")
-set participation_rate_prev (math "$prev_target_gwei / $curr_active_gwei")
+set active_gwei (echo $global_votes | jq .current_epoch_active_gwei)
+set target_gwei (echo $global_votes | jq .previous_epoch_target_attesting_gwei)
+set participation_rate (math "$target_gwei / $active_gwei")
 set prev_epoch (math "$target_epoch - 1")
 echo "Epoch: $prev_epoch"
-echo "Active GWei (as current): $prev_active_gwei"
-echo "Active GWei (as previous): $curr_active_gwei"
-echo "Matching target GWei: $prev_target_gwei"
-echo "Participation rate (as current epoch): $participation_rate_current"
-echo "Participation rate (as previous epoch): $participation_rate_prev"
+echo "Active GWei: $active_gwei"
+echo "Matching target GWei: $target_gwei"
+echo "Participation rate: $participation_rate"
